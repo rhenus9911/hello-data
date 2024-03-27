e1077bab5c5fc5a51f9f20580631f3948b2e0164
Jeonghoon <43024845+rhenus9911@users.noreply.github.com>
2024-03-27 02:22:05 +0000

    modified req.txt ,app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import altair as alt
 
 # Page title
-st.title("this is the app title")
-st.header("this is the markdown")
-st.markdown("this is the header")
-st.subheader("this is the subheader")
-st.caption("this is the caption")
-st.code("x=2021")
-st.latex(r'''a+ar^1+ar^2+ar^3''')
-
-st.checkbox('yes')
-st.button('Click')
-st.radio('Pick your gender', ['Male', 'Female'])
-st.selectbox('Pic your gender', ['Male, Female'])
-st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellect'])
-st.slider('Pick a number', 0, 50)
+rand  = np.random.normal(1, 2, size=20)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
+st.pyplot(fig)
